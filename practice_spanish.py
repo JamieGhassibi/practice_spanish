be22230f06913d51e16c7d7e7bd5cb872e18d161
Jamie Ghassibi <jamieghassibi@gmail.com>
2020-03-17 20:01:35 -0400

    Improve code comments and order

--- practice_spanish.py
+++ practice_spanish.py
@@ -4,24 +4,12 @@ import os
 
 path = os.getcwd()
 
-Translation = collections.namedtuple("Translation", "en es")
-
-def read_txt(filename):
-    """Read from specified text file."""
-    with open(os.path.join(path, filename), 'r') as lines:
-        items = [Translation(*line[:-1].split("\t")) for line in lines if line]
-    random.shuffle(items)
-    return items
-
-def write_list():
-    """Write list to python file."""
-    raise NotImplemented
-    with open(os.path.join(path, py_file), 'w') as lines:
-        pass
-
 def main():
-    phrases = read_txt("phrases.txt")
-    words = read_txt("words.txt")
+    """Extract two lists, phrases and words, then shuffle."""
+    """Iterate through the English then Spanish of each list."""
+    """The user can break the loop by entering a nonempty string."""
+    phrases = random.shuffle(read_txt("phrases.txt"))
+    words = random.shuffle(read_txt("words.txt"))
     for items in (phrases, words):
         for item in items:
             os.system('cls')
@@ -30,5 +18,17 @@ def main():
             print()
             if input(item.en): break
             if input(item.es): break
+
+def read_txt(filename):
+    """Read from specified text file into a list."""
+    Translation = collections.namedtuple("Translation", "en es")
+    with open(os.path.join(path, filename), 'r') as lines:
+        return [Translation(*line[:-1].split("\t")) for line in lines if line]
+
+def write_list():
+    """Write list to a python file to save the state of the program."""
+    raise NotImplemented
+    with open(os.path.join(path, py_file), 'w') as lines:
+        pass
 
 main()
